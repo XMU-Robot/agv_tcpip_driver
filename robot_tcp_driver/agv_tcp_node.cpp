7053a24d54e4d52ac336ec1379ec103a2c66c390
lllray <41944678+lllray@users.noreply.github.com>
2019-05-23 15:59:23 +0800

    Add files via upload

--- robot_tcp_driver/agv_tcp_node.cpp
+++ robot_tcp_driver/agv_tcp_node.cpp
@@ -6,11 +6,15 @@
 int main(int argc, char **argv) {
     ros::init(argc, argv, "sever");
     ros::NodeHandle nh;
+    ROS_INFO("-------Start!--------");
     if(!SeverInit()) return 1;
+    ROS_INFO("SeverInit is success!");
     if(!HandleInit(nh))return 1;
+    ROS_INFO("HandleInit is success!");
+    ros::Rate main_loop_rate(1000);
     while(ros::ok()){
         handle_spin();
         ros::spinOnce();
-        usleep(1000);
+        main_loop_rate.sleep();
     }
 }
--- robot_tcp_driver/include/ros_handle.h
+++ robot_tcp_driver/include/ros_handle.h
@@ -6,10 +6,15 @@
 #define AGV_TCP_DRIVER_ROS_HANDLE_H
 
 #include <ros/ros.h>
+#include <tf/transform_listener.h>
+#include <turtlesim/Spawn.h>
 #include <thread>
 #include <tcp_define.h>
 #include <geometry_msgs/Twist.h>
-
+#include "nav_msgs/OccupancyGrid.h"
+#include <move_base_msgs/MoveBaseAction.h>
+#include <actionlib/client/simple_action_client.h>
+#include <unistd.h>
 bool HandleInit(ros::NodeHandle ros_nh);
 void handle_spin();
 #endif //AGV_TCP_DRIVER_ROS_HANDLE_H
